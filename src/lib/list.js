f4b5033f73270feb8101c675f27b3bbbb1c7f9b2
Jenný Hildur Ómarsdóttir <jennyhildur@htgi-dw0025.rhi.hi.is>
2019-11-24 13:22:13 +0000

    css og sma media

--- src/lib/list.js
+++ src/lib/list.js
@@ -3,9 +3,12 @@ import { empty } from './helpers';
 export default class List {
   constructor() {
     this.container = document.querySelector('.list');
+    this.filters = document.querySelectorAll('.filters__filter');
+    this.url = 'lectures.json';
   }
 
   load() {
     empty(this.container);
   }
 }
+
