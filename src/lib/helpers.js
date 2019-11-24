ac8890ba94e14f775123f3b55c85cb793929be0c
unknown <toj6@hi.is>
2019-11-24 16:59:39 +0000

    update

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,6 +1,21 @@
-// bætti við default, ekki viss samt
-export default function empty(element) {
+export function empty(element) {
   while (element.firstChild) {
     element.removeChild(element.firstChild);
   }
+}
+
+export function el(name, ...children) {
+  const element = document.createElement(name);
+
+  if (Array.isArray(children)) {
+    children.forEach((child) => {
+      if (typeof child === 'string') {
+        element.appendChild(document.createTextNode(child));
+      } else if (child) {
+        element.appendChild(child);
+      }
+    });
+  }
+
+  return element;
 }
--- src/lib/list.js
+++ src/lib/list.js
@@ -1,27 +1,19 @@
 import { empty } from './helpers';
-export default class List {
-constructor() {
-this.container = document.querySelector('.list');
+
 
 export default class List {
   constructor() {
     this.container = document.querySelector('.list');
   }
-
+  
   load() {
     empty(this.container);
   }
 }
 
-function getData () {
-  fetch('./lectures.json') 
-  .then(res => res.json()) 
-  .then (res => console.log(res));
-}
 
-
-// eslint-disable-next-line spaced-comment
-/* "lectures": [
+/*
+ "lectures": [
   {
     "slug": "html-sagan",
     "title": "Sagan",
@@ -29,4 +21,4 @@ function getData () {
     "image": "img/code.jpg",
     "thumbnail": "img/thumb1.jpg",
     "content": [
-/*
+*/
